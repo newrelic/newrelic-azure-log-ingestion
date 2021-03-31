bb3881fbcaf7a175e7545c315a425bbc58cc5bdd
Michael Lavers <kolanos@gmail.com>
2021-03-31 11:58:52 -0700

    Adding Installation Instructions to README (#7)
    * Adding Installation Instructions to README
    
    Some of these instructions require the repo to be public to work.
    
    * Use --template-uri instead
    
    * Fix markdown formatting
    
    * Add package.json to dist directory
    
    * Add getting started section
    
    * ADd a how does it work section
    
    * Add runtime specific instrumentation links
    
    * Update template event hub name to match README

--- dist/azureLogIngestion/index.js
+++ dist/azureLogIngestion/index.js
@@ -10,23 +10,20 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const telemetry_1 = require("@newrelic/telemetry-sdk/dist/src/telemetry");
-const uuid_1 = require("uuid");
 const eventHubTrigger = function (context, eventHubMessages) {
     return __awaiter(this, void 0, void 0, function* () {
         const apiKey = process.env["NEW_RELIC_INSERT_KEY"];
         const metricClient = new telemetry_1.metrics.MetricClient({
-            apiKey
+            apiKey,
         });
         const spansClient = new telemetry_1.spans.SpanClient({
-            apiKey
+            apiKey,
         });
-        const spanBatch = new telemetry_1.spans.SpanBatch;
-        const metricBatch = new telemetry_1.metrics.MetricBatch;
+        const spanBatch = new telemetry_1.spans.SpanBatch();
+        const metricBatch = new telemetry_1.metrics.MetricBatch();
         const messages = JSON.parse(eventHubMessages[0]);
-        context.log("My messages: ", messages);
-        messages.records.forEach(message => {
-            context.log("my message: ", message);
-            const { Id, ParentId, OperationId, time, Name, DurationMs, AppRoleName, OperationName, Type, AppRoleInstance, ClientIP, SDKVersion, Success, ResourceGUID, _BilledSize, Properties = null } = message;
+        messages.records.forEach((message) => {
+            const { Id, ParentId, OperationId, time, Name, DurationMs, AppRoleName, OperationName, Type, AppRoleInstance, ClientIP, SDKVersion, Success, ResourceGUID, _BilledSize, Properties = null, } = message;
             const epochDate = new Date(time).getTime();
             const attributes = {
                 Type,
@@ -42,14 +39,6 @@ const eventHubTrigger = function (context, eventHubMessages) {
                     attributes[x] = Properties[x];
                 }
             }
-            if (OperationId === ParentId) {
-                context.log("I AM A ROOT: ", message);
-                const parentSpan = new telemetry_1.spans.Span(uuid_1.v4(), OperationId, Date.now(), 'SyntheticParentSpan', null, OperationName, 0);
-                spanBatch.addSpan(parentSpan);
-            }
-            // We've determined based on the log events we've seem from our testing in azure so far
-            // that if an ObjectId (TraceId) is the same as the ParentId then we are looking at the root span.
-            // TODO verify this or figure out if we should generate a synthetic root for our trace.
             const span = new telemetry_1.spans.Span(Id, OperationId, epochDate, Name, OperationId, OperationName, DurationMs, attributes);
             spanBatch.addSpan(span);
         });
