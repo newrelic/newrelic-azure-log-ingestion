7b6dd6c34d727b3f509a73a872d6423d27f7328a
kwest <kwest@newrelic.com>
2021-04-01 12:11:43 -0700

    Adding logging

--- azureLogIngestion/index.ts
+++ azureLogIngestion/index.ts
@@ -1,57 +1,72 @@
 import { AzureFunction, Context } from "@azure/functions"
 import { spans, metrics } from "@newrelic/telemetry-sdk/dist/src/telemetry"
 
+const apiKey = process.env["NEW_RELIC_INSERT_KEY"]
+const metricClient = new metrics.MetricClient({
+    apiKey,
+})
+const spansClient = new spans.SpanClient({
+    apiKey,
+})
+const spanBatch = new spans.SpanBatch()
+const metricBatch = new metrics.MetricBatch()
+
 const eventHubTrigger: AzureFunction = async function (context: Context, eventHubMessages: any[]): Promise<void> {
-    const apiKey = process.env["NEW_RELIC_INSERT_KEY"]
-    const metricClient = new metrics.MetricClient({
-        apiKey,
-    })
-    const spansClient = new spans.SpanClient({
-        apiKey,
-    })
-    const spanBatch = new spans.SpanBatch()
-    const metricBatch = new metrics.MetricBatch()
+    context.log(`Eventhub trigger function called for message array ${eventHubMessages}`)
 
-    const messages = JSON.parse(eventHubMessages[0])
-    messages.records.forEach((message) => {
-        const {
-            Id,
-            ParentId,
-            OperationId,
-            time,
-            Name,
-            DurationMs,
-            OperationName,
-            Type,
-            AppRoleInstance,
-            ClientIP,
-            SDKVersion,
-            Success,
-            ResourceGUID,
-            _BilledSize,
-            Properties = null,
-        } = message
+    eventHubMessages.forEach((messages) => {
+        const records = JSON.parse(messages)
 
-        const epochDate = new Date(time).getTime()
+        records.records.forEach((message) => {
+            context.log("Single event hub records message: ", message)
+            const {
+                Id,
+                ParentId,
+                OperationId,
+                time,
+                Name,
+                DurationMs,
+                OperationName,
+                Type,
+                AppRoleInstance,
+                ClientIP,
+                SDKVersion,
+                Success,
+                ResourceGUID,
+                _BilledSize,
+                Properties = null,
+            } = message
 
-        const attributes = {
-            Type,
-            AppRoleInstance,
-            ClientIP,
-            SDKVersion,
-            Success,
-            ResourceGUID,
-            BilledSize: _BilledSize,
-        }
+            const epochDate = new Date(time).getTime()
 
-        if (Properties) {
-            for (const x in Properties) {
-                attributes[x] = Properties[x]
+            const attributes = {
+                Type,
+                AppRoleInstance,
+                ClientIP,
+                SDKVersion,
+                Success,
+                ResourceGUID,
+                BilledSize: _BilledSize,
             }
-        }
 
-        const span = new spans.Span(Id, OperationId, epochDate, Name, ParentId, OperationName, DurationMs, attributes)
-        spanBatch.addSpan(span)
+            if (Properties) {
+                for (const x in Properties) {
+                    attributes[x] = Properties[x]
+                }
+            }
+
+            const span = new spans.Span(
+                Id,
+                OperationId,
+                epochDate,
+                Name,
+                ParentId === OperationId ? null : ParentId, // Determining if this is the root span or not and formatting accordingly
+                OperationName,
+                DurationMs,
+                attributes,
+            )
+            spanBatch.addSpan(span)
+        })
     })
 
     spansClient.send(spanBatch, (err) => {
