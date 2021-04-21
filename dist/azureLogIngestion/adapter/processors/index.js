1f9feaec91a9ada82c96475c5de4101f686455e9
Michael Lavers <kolanos@gmail.com>
2021-04-21 10:42:46 -0700

    Adding webpack config

--- dist/azureLogIngestion/adapter/processors/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SpanProcessor = void 0;
-var spans_1 = require("./spans");
-Object.defineProperty(exports, "SpanProcessor", { enumerable: true, get: function () { return spans_1.default; } });
-//# sourceMappingURL=index.js.map
