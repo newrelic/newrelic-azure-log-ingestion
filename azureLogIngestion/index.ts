f0cbfeb2bab4e9c3fea89d783c020a60c2cd065a
mrickard <maurice@mauricerickard.com>
2021-05-07 16:07:33 -0400

    Basic logging if DEBUG env var is set to any truthy value
    Signed-off-by: mrickard <maurice@mauricerickard.com>

--- azureLogIngestion/index.ts
+++ azureLogIngestion/index.ts
@@ -3,11 +3,15 @@ import { AzureFunction, Context } from "@azure/functions"
 import Adapter from "./adapter"
 
 const apiKey = process.env["NEW_RELIC_INSERT_KEY"]
+const debug = process.env["DEBUG"] || false
 
 const adapter = new Adapter(apiKey)
 
 const eventHubTrigger: AzureFunction = async function (context: Context, eventHubMessages: any): Promise<void> {
-    context.log(`Eventhub trigger function called for message array ${eventHubMessages}`)
+    if (debug) {
+        context.log(`Eventhub trigger function called for message eventHubMessages ${eventHubMessages}`)
+        context.log(`eventHubMessages type: ${typeof eventHubMessages}`)
+    }
     adapter.processMessages(eventHubMessages, context)
     adapter.sendBatches(context)
 }
