2d9177cb04d540bab39de5c7897a23e03d01c867
Michael Lavers <kolanos@gmail.com>
2021-05-19 08:54:36 -0700

    Set service.name to Azure Resource Name

---
+++ azureLogIngestion/adapter/utils/resource.test.ts
@@ -0,0 +1,31 @@
+import { hash, parse } from "./resource"
+
+describe("hash", () => {
+    it("will hash an entity", () => {
+        expect(
+            hash(
+                123456,
+                "INFRA",
+                "AZUREFUNCTIONAPP",
+                // /subscriptions/{subscription}/resourceGroups/{resourceGroup}/{provider}/{resourceType}/{resourceName}
+                "/subscriptions/62767007-2c55-4c76-926e-e8482f4e7f25/resourceGroups/test-group/providers/Microsoft.Web/sites/test-function",
+            ),
+        ).toBe("MTIzNDU2fElORlJBfEFaVVJFRlVOQ1RJT05BUFB8MTE3MjcxOTQ5MTAyOTQ5MTc0MjM")
+    })
+})
+
+describe("parse", () => {
+    it("will parse a resource id", () => {
+        expect(
+            parse(
+                "/subscriptions/62767007-2c55-4c76-926e-e8482f4e7f25/resourceGroups/test-group/providers/Microsoft.Web/sites/test-function",
+            ),
+        ).toMatchObject({
+            subscription: "62767007-2c55-4c76-926e-e8482f4e7f25",
+            resourceGroup: "test-group",
+            provider: "Microsoft.Web",
+            resourceType: "sites",
+            resourceName: "test-function",
+        })
+    })
+})
---
+++ azureLogIngestion/adapter/utils/resource.ts
@@ -0,0 +1,29 @@
+import { hash64 } from "farmhash"
+
+const urlSafeChars = { "+": "-", "/": "_", "=": "" }
+
+/**
+ * Hashes an Azure Resource ID into  a New Relic Entity GUID
+ */
+export function hash(accountId: number, domain: string, type: string, resourceId: string): string {
+    const resourceHash = hash64(new Buffer(resourceId))
+    return new Buffer(`${accountId}|${domain}|${type}|${resourceHash}`)
+        .toString("base64")
+        .replace(/[\+\/=]/, (c) => urlSafeChars[c])
+}
+
+export interface Resource {
+    subscription: string
+    resourceGroup: string
+    provider: string
+    resourceType: string
+    resourceName: string
+}
+
+/**
+ * Parses an Azure Resource ID into its respective parts
+ */
+export function parse(resourceId: string): Resource {
+    const [, , subscription, , resourceGroup, , provider, resourceType, resourceName] = resourceId.split("/")
+    return { subscription, resourceGroup, provider, resourceType, resourceName }
+}
