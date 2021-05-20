29385fa8a812640d3d597c99de25f710f3f2629b
Maurice Rickard <maurice@mauricerickard.com>
2021-05-20 16:03:23 -0400

    Removing farmhash for now--our build pipeline builds on ubuntu and ships (potentially) to Windows (#50)
    Signed-off-by: mrickard <maurice@mauricerickard.com>

--- azureLogIngestion/adapter/utils/resource.test.ts
+++ azureLogIngestion/adapter/utils/resource.test.ts
@@ -10,7 +10,7 @@ describe("hash", () => {
                 // /subscriptions/{subscription}/resourceGroups/{resourceGroup}/{provider}/{resourceType}/{resourceName}
                 "/subscriptions/62767007-2c55-4c76-926e-e8482f4e7f25/resourceGroups/test-group/providers/Microsoft.Web/sites/test-function",
             ),
-        ).toBe("MTIzNDU2fElORlJBfEFaVVJFRlVOQ1RJT05BUFB8MTE3MjcxOTQ5MTAyOTQ5MTc0MjM")
+        ).toBe("MTIzNDU2fElORlJBfEFaVVJFRlVOQ1RJT05BUFB8")
     })
 })
 
