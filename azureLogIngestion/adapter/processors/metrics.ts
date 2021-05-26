33c384fb99ba46af6d1d4edffad1355dccc54038
mrickard <mrickard@newrelic.com>
2021-05-26 15:07:58 -0400

    Adding performance counters, fixing comment typo in logs
    Signed-off-by: mrickard <mrickard@newrelic.com>

--- azureLogIngestion/adapter/processors/metrics.ts
+++ azureLogIngestion/adapter/processors/metrics.ts
@@ -4,13 +4,13 @@ import { Context } from "@azure/functions"
 import { Processor } from "./base"
 import flatten from "../utils/flatten"
 
-export default class LogsProcessor implements Processor {
-    client: telemetry.logs.LogClient
-    batch: telemetry.logs.LogBatch
+export default class MetricsProcessor implements Processor {
+    client: telemetry.metrics.MetricClient
+    batch: telemetry.metrics.MetricBatch
 
     constructor(apiKey: string) {
         const host = process.env.NEW_RELIC_REGION === "eu" ? "log-api.eu.newrelic.com" : "log-api.newrelic.com"
-        this.client = new telemetry.logs.LogClient({ apiKey, host })
+        this.client = new telemetry.metrics.MetricClient({ apiKey, host })
         this.startNewBatch()
     }
 
@@ -18,31 +18,43 @@ export default class LogsProcessor implements Processor {
      * Starts a new batch, setting common attributes
      */
     private startNewBatch(): void {
-        this.batch = new telemetry.logs.LogBatch([], { "cloud.provider": "azure" })
+        this.batch = new telemetry.metrics.MetricBatch({ "cloud.provider": "azure" })
+    }
+
+    private setMetricType(metric: any): string {
+        const counters = ["Process", "process"]
+        const gauges = ["Memory", "memory"]
+
+        if (gauges.includes(metric.Category)) {
+            return "gauge"
+        }
+        if (counters.includes(metric.Category)) {
+            return "counter"
+        }
+        return "counter"
     }
 
     /**
-     * Processes a log message and add to current batch
+     * Processes a metric and add to current batch
      */
     processMessage(message: Record<string, any>, context: Context): void {
         // Deleting attributes we do not want to send to New Relic
         // TODO: Make this a part of a processor attribute filter method
         delete message.iKey
 
-        const { type, timestamp, properties = {}, operationId, ...rest } = message
-        const logtype = `Azure${type}`
+        const { type, timestamp, properties, name, category, counter, value, resourceGuid, ...rest } = message
         const epochDate = new Date(timestamp).getTime()
-        const traceId = operationId
         const attributes = {
-            ...flatten({ logtype, ...properties, ...rest, timestamp: epochDate, operationId, "trace.id": traceId }),
+            ...flatten({ ...properties, ...rest, timestamp: epochDate }),
         }
 
-        const log = new telemetry.logs.Log(attributes.message, epochDate, attributes)
-        this.batch.append(log)
+        // no interval MS on performanceCounters, it seems
+        const metric = new telemetry.metrics.CountMetric(name, value, attributes, epochDate)
+        this.batch.addMetric(metric)
     }
 
     /**
-     * Sends log batches to New Relic
+     * Sends event telemetry batches to New Relic
      *
      * Currently this doesn't create a new batch on failure, allowing for
      * reattempts. This will result in a memory leak for subsequent failures.
