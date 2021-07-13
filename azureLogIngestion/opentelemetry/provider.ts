93d9c7ed18b3e02b1b873751e70949f7469ec985
mrickard <maurice@mauricerickard.com>
2021-07-13 11:42:42 -0400

    Trying explicitly setting resource accessor scope
    Signed-off-by: mrickard <maurice@mauricerickard.com>

--- azureLogIngestion/opentelemetry/provider.ts
+++ azureLogIngestion/opentelemetry/provider.ts
@@ -2,12 +2,19 @@ import { Resource } from "@opentelemetry/resources"
 import { BasicTracerProvider } from "@opentelemetry/tracing"
 
 export class NRTracerProvider extends BasicTracerProvider {
-    azureResource: Resource
+    constructor(props) {
+        super(props)
+        this["resource"] = this.resource
+    }
+
+    private _azureResource: Resource
 
     public get resource(): Resource {
-        return this.azureResource
+        return this._azureResource
     }
     public set resource(resource: Resource) {
-        this.azureResource = resource
+        this._azureResource = resource
     }
 }
+
+class NRTracerProviderImpl extends NRTracerProvider {}
