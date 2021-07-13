eb8dca111f29f19b5c07bf89b7702ccb19460e48
Michael Lavers <kolanos@gmail.com>
2021-07-12 21:08:06 -0700

    Add a ts-ignore comment, not getting this error locally

--- azureLogIngestion/opentelemetry/provider.ts
+++ azureLogIngestion/opentelemetry/provider.ts
@@ -2,6 +2,7 @@ import { Resource } from "@opentelemetry/resources"
 import { BasicTracerProvider } from "@opentelemetry/tracing"
 
 export class NRTracerProvider extends BasicTracerProvider {
+    // @ts-ignore
     resource: Resource
 
     updateResource(resource: Resource): void {
