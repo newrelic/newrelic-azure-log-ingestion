baf4f9597379b0442eb29e43adcf2ea13694db82
mrickard <maurice@mauricerickard.com>
2021-08-06 19:04:02 -0400

    Additional logging to debug timing/duration, and account for different timing from App Insights
    Signed-off-by: mrickard <mrickard@newrelic.com>

--- azureLogIngestion/utils/time.ts
+++ azureLogIngestion/utils/time.ts
@@ -1,23 +1,32 @@
-export const convertToMs = (interval: string): number => {
+import { Context as AzureContext } from "@azure/functions"
+
+export const convertToMs = (interval: string, ctx?: AzureContext): number => {
     const scale = String(interval).match(/[a-zA-Z]+/g)
     const intervalNumber = String(interval).match(/[0-9.]+/g)
     let ms
+    if (!interval) {
+        return 0
+    }
     if (!scale) {
+        // ctx.log("no scale", interval)
         return Number(interval)
     }
     const units = scale[0].toLowerCase()
+    // ctx.log(`UNITS ${units}`, interval, scale[0])
     if (units === "ms") {
         ms = Number(intervalNumber[0])
     } else if (units === "s") {
         ms = Number(intervalNumber[0]) * 1000
     } else if (units === "m") {
         ms = Number(intervalNumber[0]) * 1000 * 60
+    } else {
+        return Number(interval)
     }
     return ms
 }
 
-export const endTimeFromDuration = (timestamp: string, duration: string): Date => {
+export const endTimeFromDuration = (timestamp: string, duration: string, ctx?: AzureContext): Date => {
     const dateTime = new Date(timestamp).getTime()
-    const elapsed = convertToMs(duration)
+    const elapsed = convertToMs(duration, ctx)
     return new Date(dateTime + elapsed)
 }
