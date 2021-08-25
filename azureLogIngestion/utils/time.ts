088abc6f8f3eb00890f8e5b41594a878e3190492
Maurice Rickard <maurice@mauricerickard.com>
2021-08-24 20:27:19 -0400

    fix endtime calculation (#68)
    * Additional logging to debug timing/duration, and account for different timing from App Insights
    
    Signed-off-by: mrickard <mrickard@newrelic.com>
    
    * Converting endtimes to hrTime with OT core convenience method
    
    Signed-off-by: mrickard <maurice@mauricerickard.com>
    
    * Removing debugging context arg
    
    Signed-off-by: mrickard <maurice@mauricerickard.com>

--- azureLogIngestion/utils/time.ts
+++ azureLogIngestion/utils/time.ts
@@ -1,7 +1,14 @@
+import { Context as AzureContext } from "@azure/functions"
+import { timeInputToHrTime } from "@opentelemetry/core"
+import { HrTime } from "@opentelemetry/api"
+
 export const convertToMs = (interval: string): number => {
     const scale = String(interval).match(/[a-zA-Z]+/g)
     const intervalNumber = String(interval).match(/[0-9.]+/g)
     let ms
+    if (!interval) {
+        return 0
+    }
     if (!scale) {
         return Number(interval)
     }
@@ -12,12 +19,15 @@ export const convertToMs = (interval: string): number => {
         ms = Number(intervalNumber[0]) * 1000
     } else if (units === "m") {
         ms = Number(intervalNumber[0]) * 1000 * 60
+    } else {
+        return Number(interval)
     }
     return ms
 }
 
-export const endTimeFromDuration = (timestamp: string, duration: string): Date => {
+export const endTimeFromDuration = (timestamp: string, duration: string): HrTime => {
     const dateTime = new Date(timestamp).getTime()
     const elapsed = convertToMs(duration)
-    return new Date(dateTime + elapsed)
+    const endTime = new Date(dateTime + elapsed)
+    return timeInputToHrTime(endTime)
 }
